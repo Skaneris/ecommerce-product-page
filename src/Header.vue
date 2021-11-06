39c0a67c799d9d1a5a08a0f917fa9ddb591981e1
Sergey Prijan <dneprserp355@gmail.com>
2021-11-06 05:38:17 +0200

    initial commit

---
+++ src/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import Header from './Header.vue'
 import App from './App.vue'
+import './assets/sass/app.sass'
 
+createApp(Header).mount('#header')
 createApp(App).mount('#app')
