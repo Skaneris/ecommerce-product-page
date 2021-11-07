c6033a4e73637efd25dea5203c32d35a74a67039
Sergey Prijan <dneprserp355@gmail.com>
2021-11-08 00:36:42 +0200

    create dist

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import store from './store'
-import Header from './Header.vue'
-import App from './App.vue'
-import './assets/sass/app.sass'
-
-createApp(Header).use(store).mount('#header')
-createApp(App).use(store).mount('#main')
